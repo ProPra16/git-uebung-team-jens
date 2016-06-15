cc55b3df3a32da0560f7c21687fd61653bcbf60b
marvindukome <marvin-dukome@t-online.de>
2016-06-15 11:39:00 +0200

    add button

--- src/de/hhu/propra16/FiveButtonsController.java
+++ src/de/hhu/propra16/FiveButtonsController.java
@@ -1,7 +1,15 @@
 package de.hhu.propra16;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 /**
  * Created by Tobias Witt on 06.06.16.
  */
 public class FiveButtonsController {
+	@FXML
+	public void useButton(ActionEvent event){
+		System.exit(0);
+		
+	}
 }
